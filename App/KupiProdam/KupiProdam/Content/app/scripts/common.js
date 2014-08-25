12b1744ae830a1c77c72d4bec045b14d901d4fdc
Vidrickiy <vidrickiy@bars-open.ru>
2014-08-25 17:35:38 +0400

    * Для регистрации сделал скрываемую левую часть и сломал окно входа

--- App/KupiProdam/KupiProdam/Content/app/scripts/common.js
+++ App/KupiProdam/KupiProdam/Content/app/scripts/common.js
@@ -6,4 +6,15 @@ function showLogin() {
     $('#login').on('click', function () {
         $('#loginForm').modal();
     });
+
+    $('.modal').on('shown.bs.modal', function () {
+        $(this).find('.modal-dialog').css({
+            'margin-top': function () {
+                return -($(this).outerHeight() / 2);
+            },
+            'margin-left': function () {
+                return -($(this).outerWidth() / 2);
+            }
+        });
+    });
 }
